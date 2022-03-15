a7bbc4abfc828930304003db1c282d34196af1d8
ryanmuhl <ryanmuhl@hotmail.com>
2022-03-14 21:50:20 -0400

    complete logic for fetch api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 var songContainer = document.querySelector("#song-list");
+var videoContainer = document.querySelector("#text-list");
+
 var songListArray = [];
 //Click button then fetch artist data
 $("#click-celebrity").click(function () {
   getArtistData();
+  
 });
 
 //Function to fetch artist data
@@ -31,13 +34,17 @@ var getArtistData = function () {
 
       for (var i = 0; i < allSongs.length; i++) {
         var artistSongList = allSongs[i].result.title;
-        console.log(artistSongList)
+        // console.log(artistSongList)
 
         var artistSongs = document.createElement("li");
         var artistSongsText = document.createElement("a")
         artistSongsText.textContent = artistSongList;
 
-        artistSongsText.href = "#" + artistSongList;
+        //artistSongsText.href = "artistLink" ;
+        
+        artistSongsText.setAttribute('id', "artist-" + i);
+
+        
 
 
 
@@ -48,13 +55,77 @@ var getArtistData = function () {
 
         songListArray.push(artistSongList);// push artist song list into this array
       }
+
       localStorage.setItem("text", JSON.stringify(songListArray));
+
+      for (i = 0; i < 10; i++) {
+      document.getElementById("artist-"+ i).addEventListener("click", nextAPI);
+    }
 
       //  store for loop to local storage here - localStorage.setItem(text,jSON.stringify(the array)) artist name is key = array
     });
 };
+function nextAPI(event){
+ //recieve an event
+ //get event text
+ //format text
+ //get song link with formatted text
+
+ var songName = event.target.text
+//  console.log(songName)
+  getSongLink(songName)
+
+}
+
+// var getSongLink = function(kndkwqndparam) {
+function getSongLink(param){
+
+  //receive paramater song name
+  //insert parameter in request url link
+
+  var requestUrlLink = "https://shazam.p.rapidapi.com/search?term="+ param +"&locale=en-US&offset=0&limit=5"
+  
+  fetch(requestUrlLink, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "shazam.p.rapidapi.com",
+      "x-rapidapi-key": "20462df53amsh8d0409a5e8b7f2fp1522cejsn8fa1bed5cd51"
+    }
+  })
+  .then(function (response) {
+    // console.log(response)
+    return response.json();
+  })
+  .then (function(data) {
+
+    var textList = data.tracks.hits
+    // console.log (textList)
+
+    
+      var videoTextList = textList[0].track.share.href;
+     
+
+      var textListVideo = document.createElement("li");
+      
+      videoContainer.append(textListVideo)
+      textListVideo.append(videoTextList)
+      
+      
+
+      
 
 
+
+      // songContainer.append(artistSongs);
+      // artistSongs.append(artistSongsText)
+
+
+
+      // songListArray.push(artistSongList);// push artist song list into this array
+    
+
+  });
+};
 
 //Fetch API info
 
@@ -82,7 +153,6 @@ var getArtistData = function () {
 
 
 //pull from local storage and append to second html page
-
 
 
 
