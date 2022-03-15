daf11433035d493c82577dd1ad61f9eb161fdd85
Chlovanna <ashdmt39@gmail.com>
2022-03-15 18:42:43 -0400

    add additional button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
-<<<<<<< HEAD
 var songContainer = document.querySelector("#song-list");
+var videoContainer = document.querySelector("#text-list");
+
 var songListArray = [];
-//Click button then fetch artist data
+//Click button to fetch Artist Data
 $("#click-celebrity").click(function () {
   getArtistData();
 });
 
-//Function to fetch artist data
+//Function to fetch data from Genius API
 var getArtistData = function () {
-  var text = $("#type-artist-name").val();
+  text = $("#type-artist-name").val();
 
   var requestUrl = "https://genius.p.rapidapi.com/search?q=" + text;
 
@@ -20,95 +21,73 @@ var getArtistData = function () {
     },
   })
     .then(function (response) {
+      console.log(response);
       return response.json();
     })
+
     .then(function (data) {
-      var allSongs = data.response.hits; // are we only display the hits of the artist? we may need to make this clear to the user; Rihanna for example has over ten songs
-      console.log(allSongs);
+      var allSongs = data.response.hits;
+
       for (var i = 0; i < allSongs.length; i++) {
-        var artistSongList = allSongs[i].result.title; 
+        var artistSongList = allSongs[i].result.title;
 
         var artistSongs = document.createElement("li");
+        var artistSongsText = document.createElement("a");
+        artistSongsText.textContent = artistSongList;
 
-        artistSongs.textContent = artistSongList;
-        console.log(artistSongList);
+        artistSongsText.setAttribute("id", "artist-" + i);
+
         songContainer.append(artistSongs);
-        songListArray.push(artistSongList);// push artist song list into this array
+        artistSongs.append(artistSongsText);
+
+        songListArray.push(artistSongList);
       }
+
       localStorage.setItem("text", JSON.stringify(songListArray));
+
+      for (i = 0; i < 10; i++) {
+        //click event to target feth data from Genius API
+        document
+          .getElementById("artist-" + i)
+          .addEventListener("click", nextAPI);
+      }
 
       //  store for loop to local storage here - localStorage.setItem(text,jSON.stringify(the array)) artist name is key = array
     });
 };
 
-// if(songContainer.hasChildren()){
-//   songContainer.empty();
-//   ---run the function to get the info---
-//   } else {
-//   ---run the function to get the info---
-//   }
+//Function to target click event within get Artist Data and pass songName
+//to getSongLink parameter
+function nextAPI(event) {
+  var songName = event.target.text;
+  getSongLink(songName);
+}
 
-// function clearHistory(event){
-//   event.preventDefault();
-//   songListArray=[];
-//   localStorage.removeItem("text");
-//   document.location.reload();
+//Function to Fetch Shazam API
+function getSongLink(param) {
+  var requestUrlLink =
+    "https://shazam.p.rapidapi.com/search?term=" +
+    param +
+    "&locale=en-US&offset=0&limit=5";
 
-//create an empty array to store all song titles, then after the for loop is complete save the array to local and give the array key of artist names//
-//one array for the song titles, and second for the artist name another for the artist
+  fetch(requestUrlLink, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "shazam.p.rapidapi.com",
+      "x-rapidapi-key": "20462df53amsh8d0409a5e8b7f2fp1522cejsn8fa1bed5cd51",
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var textList = data.tracks.hits;
+      var videoTextList = textList[0].track.share.href;
+      var textListVideo = document.createElement("li");
 
-//Fetch API info
+      videoContainer.append(textListVideo);
+      textListVideo.append(videoTextList);
 
-//Function/Logic
-
-//Make one function that makes call to shazam API and One functon that makes a call to the Genius API
-
-//Need to make two API calls to get to genius
-
-//User Input Variable will determine what information is pulled from the Genius API
-
-//User Input Variable will determine what Lyrics will be pulled from the Shazam API
-
-//Append the Data to the page
-
-//Save to Local Storage
-
-//pull from local storage and append to second html page
-=======
-//Research API Documentation
-
-
-//Add API scripts
-
-
-//Need to declare Variables one Being a Function
-
-
-//Query Selector for click me Buttons
-
-
-
-//Fetch API info
-
-
-//Function/Logic 
-
-
-//User Input Variable will determine what information is pulled from the Genius API
-
-
-//User Input Variable will determine what Lyrics will be pulled from the Shazam API
-
-
-//Append the Data to the page
-
-
-//Save to Local Storage
-
-
-//pull from local storage and append to second html page
-
-
-
-
->>>>>>> 7d7801a899ca3f1ee881d2b803340e9a6328dd69
+      // songListArray.push(artistSongList);// push artist song list into this array
+    });
+}
