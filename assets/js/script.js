408c3e97e7ae2a68ca7e5ffcdedaf6d5e3aef469
ryanmuhl <ryanmuhl@hotmail.com>
2022-03-15 18:20:15 -0400

    add clear button to list form

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,13 @@ var songContainer = document.querySelector("#song-list");
 var videoContainer = document.querySelector("#text-list");
 
 var songListArray = [];
-//Click button then fetch artist data
+//Click button to fetch Artist Data
 $("#click-celebrity").click(function () {
   getArtistData();
   
 });
 
-//Function to fetch artist data
+//Function to fetch data from Genius API
 var getArtistData = function () {
   text = $("#type-artist-name").val();
 
@@ -28,60 +28,52 @@ var getArtistData = function () {
     })
     
     .then(function (data) {
-      var allSongs = data.response.hits; // are we only display the hits of the artist? we may need to make this clear to the user; Rihanna for example has over ten songs
-      // console.log(data)
-      // console.log(allSongs)
+      var allSongs = data.response.hits;
 
       for (var i = 0; i < allSongs.length; i++) {
         var artistSongList = allSongs[i].result.title;
-        // console.log(artistSongList)
+       
 
         var artistSongs = document.createElement("li");
         var artistSongsText = document.createElement("a")
         artistSongsText.textContent = artistSongList;
 
-        //artistSongsText.href = "artistLink" ;
         
         artistSongsText.setAttribute('id', "artist-" + i);
 
         
-
-
-
         songContainer.append(artistSongs);
         artistSongs.append(artistSongsText)
 
 
-
-        songListArray.push(artistSongList);// push artist song list into this array
+        songListArray.push(artistSongList);
       }
 
       localStorage.setItem("text", JSON.stringify(songListArray));
 
       for (i = 0; i < 10; i++) {
+
+       //click event to target feth data from Genius API 
       document.getElementById("artist-"+ i).addEventListener("click", nextAPI);
     }
 
       //  store for loop to local storage here - localStorage.setItem(text,jSON.stringify(the array)) artist name is key = array
     });
 };
+
+//Function to target click event within get Artist Data and pass songName
+//to getSongLink parameter
 function nextAPI(event){
- //recieve an event
- //get event text
- //format text
- //get song link with formatted text
+
 
  var songName = event.target.text
-//  console.log(songName)
   getSongLink(songName)
 
 }
 
-// var getSongLink = function(kndkwqndparam) {
+//Function to Fetch Shazam API
 function getSongLink(param){
 
-  //receive paramater song name
-  //insert parameter in request url link
 
   var requestUrlLink = "https://shazam.p.rapidapi.com/search?term="+ param +"&locale=en-US&offset=0&limit=5"
   
@@ -93,33 +85,17 @@ function getSongLink(param){
     }
   })
   .then(function (response) {
-    // console.log(response)
+   
     return response.json();
   })
   .then (function(data) {
 
     var textList = data.tracks.hits
-    // console.log (textList)
-
-    
-      var videoTextList = textList[0].track.share.href;
-     
-
-      var textListVideo = document.createElement("li");
+    var videoTextList = textList[0].track.share.href;
+    var textListVideo = document.createElement("li");
       
       videoContainer.append(textListVideo)
       textListVideo.append(videoTextList)
-      
-      
-
-      
-
-
-
-      // songContainer.append(artistSongs);
-      // artistSongs.append(artistSongsText)
-
-
 
       // songListArray.push(artistSongList);// push artist song list into this array
     
@@ -127,32 +103,13 @@ function getSongLink(param){
   });
 };
 
-//Fetch API info
 
 
 
-//Function/Logic
 
 
-//Make one function that makes call to shazam API and One functon that makes a call to the Genius API
 
 
-//Need to make two API calls to get to genius
-
-
-//User Input Variable will determine what information is pulled from the Genius API
-
-
-//User Input Variable will determine what Lyrics will be pulled from the Shazam API
-
-
-//Append the Data to the page
-
-
-//Save to Local Storage
-
-
-//pull from local storage and append to second html page
 
 
 
