579e849dddcc228ce1741a46d29dc45111a73ae8
Chlovanna <ashdmt39@gmail.com>
2022-03-19 13:07:14 -0400

    final touch

--- README.md
+++ README.md
@@ -1,38 +1,68 @@
-MUSIC-LOVER-PROJECT1
-Group Project 1
-Purpose
+# MUSIC-LOVER-PROJECT1
+
+- Group Project 1
+
+## Purpose
+
 Create a Front End application incorporating skills learned throughout 1st portion of Coding Bootcamp.
 
-User Story
-As a Music Lover I want an application where I am able to input a Music Artist/Band Name and display a list of their top 10 hits So that I can create a diverse play list, including a link to to song content allowing user to listen to song.
+## User Story
 
-Website Functionality/Class Criteria Met
-When User Types in Artist/Band Name then selects Click button, A list of Top 10 songs for that artist will append to the designated song List field. Each song is designated as a clickable link.
-When button is clicked and top 10 songs are displayed, the list is also saved to local storage.
-When a specific song within the list is selected, a link will append to a second field. This link will take the user to the shazam video/url where user can listen to song.
-When user selects a song link, the video/url link should save to local storage.
-Ice-Box
-Built With
-HTML
-CSS
-JAVA SCRIPT
-JQUERY
-BULMA
-IONICONS
-API
-Rapid API - Genius
-Rapid API - Shazam
-Website URL
+As a Music Lover
+I want an application where I am able to input a Music Artist/Band Name and display a list of their top 10 hits
+So that I can create a diverse play list, including a link to to song content allowing user to listen to song.
+
+## Website Functionality/Class Criteria Met
+
+- When User Types in Artist/Band Name then selects Click For Top 10 Songs button, A list of Top 10 songs for that artist will append to the See List Of Songs Field. Each of these songs is a clickable link.
+- When a specific song within the list is selected, a link will append to a second field (Select Shazams Song Link To Listen To Song). This link will take the user to the shazam video/url where user can listen to song. This click event will also append the song to playlist field and save to local storage. Local Storage is persistant on page reload.
+- When Clear Lists button is selected, See List OF Songs and Select Shazam Song content will clear. Local storage and playlist will remain until clear playlist button is selected.
+- When Clear Playlist button is selected, all content on page will clear (including local storage), and page will re-load.
+
+# Ice-Box/Future Functionality
+
+- Future functionality will include abilitiy within playlist field to delete unwanted songs.
+- Future functionality will include a picture of the album associated with the song
+- Future functionality will include the ability to play song directy from web page within playlist
+
+## Built With
+
+- HTML
+- CSS
+- JAVA SCRIPT
+- JQUERY
+- BULMA
+- IONICONS
+
+## API
+
+- Rapid API - Genius
+- Rapid API - Shazam
+
+## Website URL
+
 https://ryanmuhl.github.io/music-lover-project1/
 
-GitHub Repository
+## GitHub Repository
+
 https://github.com/ryanmuhl/music-lover-project1
 
-Screenshot
-Challenge Screenshot
+## Screenshot
 
-Contributions
-Created/Designed by: Ryan Uhl Ashley Lawrence David McKinney
+![Challenge Screenshot](https://github.com/ryanmuhl/music-lover-project1/blob/feature/variables/assets/images/music-lover-screenshot.png.jpg)
 
-Email: ryanmuhl@hotmail.com
-Email: ashdmt39@gmail.com
+## Contributions
+
+Created/Designed by:
+
+- Student: Ashley Lawrence
+- Email: ashdmt39@gmail.com
+- Portfolio Repository: https://chlovanna.github.io/The-Asset-Why-Not/
+
+- Student: David McKinney
+- Email: mckdav98@gail.com
+- Portfolio Repository: https://github.com/DAVMCKII/my-portfolio
+
+- Student: Ryan Uhl
+- Email: ryanmuhl@hotmail.com
+- Portfolio Repository: https://github.com/ryanmuhl/ryanmuhl-portfolio
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
+//Declair Global Variables
 var songContainer = document.querySelector("#song-list");
 var videoContainer = document.querySelector("#text-list");
+var list = document.querySelector("#favorites");
 
-var songListArray = [];
-//Click button to fetch Artist Data
+//Click button to fetch Artist Data/Top 10 Songs for Artist
 $("#click-celebrity").click(function () {
   getArtistData();
 });
@@ -21,44 +22,43 @@ var getArtistData = function () {
     },
   })
     .then(function (response) {
-      console.log(response);
       return response.json();
     })
 
     .then(function (data) {
       var allSongs = data.response.hits;
 
+      $(songContainer).empty();
       for (var i = 0; i < allSongs.length; i++) {
         var artistSongList = allSongs[i].result.title;
 
+        //Append List of top 10 songs to DIV/UL
+        //Assign each Li an unique id
         var artistSongs = document.createElement("li");
         var artistSongsText = document.createElement("a");
         artistSongsText.textContent = artistSongList;
 
         artistSongsText.setAttribute("id", "artist-" + i);
 
-        songContainer.append(artistSongs);
         artistSongs.append(artistSongsText);
-
-        songListArray.push(artistSongList);
+        songContainer.append(artistSongs);
       }
 
-      localStorage.setItem("text", JSON.stringify(songListArray));
-
+      //Create Click Event for each Song within top 10 Song List
+      //This allows user to click on song and fetch Shazam API info
       for (i = 0; i < 10; i++) {
         //click event to target feth data from Genius API
         document
           .getElementById("artist-" + i)
           .addEventListener("click", nextAPI);
       }
-
-      //  store for loop to local storage here - localStorage.setItem(text,jSON.stringify(the array)) artist name is key = array
     });
 };
 
 //Function to target click event within get Artist Data and pass songName
 //to getSongLink parameter
 function nextAPI(event) {
+  $("#text-list").empty();
   var songName = event.target.text;
   getSongLink(songName);
 }
@@ -74,7 +74,7 @@ function getSongLink(param) {
     method: "GET",
     headers: {
       "x-rapidapi-host": "shazam.p.rapidapi.com",
-      "x-rapidapi-key": "20462df53amsh8d0409a5e8b7f2fp1522cejsn8fa1bed5cd51",
+      "x-rapidapi-key": "ac9ab99d33msh3d2e68d58bcbf83p143df8jsn672a2b9e440d",
     },
   })
     .then(function (response) {
@@ -84,10 +84,57 @@ function getSongLink(param) {
       var textList = data.tracks.hits;
       var videoTextList = textList[0].track.share.href;
       var textListVideo = document.createElement("li");
+      var textListVideoA = document.createElement("a");
+      textListVideoA.setAttribute("href", videoTextList);
+      textListVideoA.setAttribute("target", "_blank");
 
+      //Append song link/url to song link field
       videoContainer.append(textListVideo);
-      textListVideo.append(videoTextList);
+      textListVideo.append(textListVideoA);
+      textListVideoA.append(videoTextList);
 
-      // songListArray.push(artistSongList);// push artist song list into this array
+      loadingPlaylistText(videoTextList);
     });
 }
+
+//Function to keep local storage persistant when page reloads (Song List remains when page reloads)
+function loadingPlaylistText(songUrl) {
+  var songListArray = JSON.parse(window.localStorage.getItem("text")) || [];
+
+  if (songUrl) {
+    songListArray.push(songUrl);
+  }
+
+  localStorage.setItem("text", JSON.stringify(songListArray));
+
+  saveClick(songListArray);
+}
+
+//function to Save to local storage when an individual song link is selected
+function saveClick(info) {
+  $(list).empty();
+  for (i = 0; i < info.length; i++) {
+    var listContent = document.createElement("li");
+    listContent.textContent = info[i];
+    list.append(listContent);
+  }
+}
+
+//function to Clear Playlist/Local Storage when button is selected (Also reloads browser)
+$("#clear-playlist").click(function () {
+  if ("clear-playlist") {
+    $(favorites).empty();
+  }
+  localStorage.clear();
+  window.location.reload();
+});
+
+//function to Clear List of Songs and URL (Also reloads browser)
+$("#clear").click(function () {
+  var clearAllLists = document.getElementById("song-list");
+  $(clearAllLists).empty();
+  $(clear).empty();
+  window.location.reload();
+});
+
+loadingPlaylistText();
